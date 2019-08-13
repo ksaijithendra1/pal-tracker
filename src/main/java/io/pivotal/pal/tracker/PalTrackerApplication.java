9a29f8c773ab3eeb5cdff23fab8d980b9922be00
Sai Jithendra <saijithendra.kodavaluru@gmail.com>
2019-08-13 16:12:58 -0400

    JDBC Changes

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -4,13 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
 public class PalTrackerApplication {
 
+//    @Bean TimeEntryRepository
+//    TimeEntryRepository() {
+//        return new InMemoryTimeEntryRepository();
+//    }
+
     @Bean TimeEntryRepository
-    TimeEntryRepository() {
-        return new InMemoryTimeEntryRepository();
+    TimeEntryRepository(DataSource datasource) {
+        return new JdbcTimeEntryRepository(datasource);
     }
+
 
     public static void main(String[] args) {
         SpringApplication.run(PalTrackerApplication.class, args);
